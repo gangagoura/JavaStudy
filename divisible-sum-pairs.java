a9271db52b2264755e83281f0dd4f1e398af8a91
GUILHERME FERNANDES NETO <40431403+gangagoura@users.noreply.github.com>
2023-03-19 12:19:25 -0300

    Create divisible-sum-pairs.java

---
+++ divisible-sum-pairs.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int cnt=0;
+        for (int i = 0 ;i<n-1;i++) {
+            for (int j=i+1;j<n;j++) {
+                if ((a[i]+a[j]) % k == 0)
+                    cnt ++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
